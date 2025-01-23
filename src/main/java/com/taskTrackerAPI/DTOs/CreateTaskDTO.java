7c3f362e5e0899a4a98a15717fd6f6cfce6d732f
Filipe Silva <118104038+FilipeSilva21@users.noreply.github.com>
2025-01-23 17:13:46 -0300

    Delete src directory

--- src/main/java/com/taskTrackerAPI/DTOs/CreateTaskDTO.java
+++
@@ -1,4 +0,0 @@
-package com.taskTrackerAPI.DTOs;
-
-public record CreateTaskDTO(String name, String description, Boolean isDone, Long userId) {
-}
--- src/main/java/com/taskTrackerAPI/DTOs/TaskResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package com.taskTrackerAPI.DTOs;
-
-public record TaskResponseDTO (Long userId, String name, String description, Boolean isDone){
-}
