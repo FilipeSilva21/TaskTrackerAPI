d42aac810916f30b6f449ab27dff5ea40219ae2d
Filipe Silva <118104038+FilipeSilva21@users.noreply.github.com>
2025-01-29 19:19:22 -0300

    Add files via upload

---
+++ src/main/java/com/taskTrackerAPI/entities/Task.java
@@ -0,0 +1,82 @@
+package com.taskTrackerAPI.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tbTasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "taskId")
+    private Long taskId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "isDone")
+    private Boolean isDone;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    @JsonIgnoreProperties("tasks")
+    public User user;
+
+    public Task() {
+    }
+
+    public Task(Long taskId, String name, String description, Boolean isDone, User user) {
+        this.taskId = taskId;
+        this.name = name;
+        this.description = description;
+        this.isDone = isDone;
+        this.user = user;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getDone() {
+        return isDone;
+    }
+
+    public void setDone(Boolean done) {
+        this.isDone = done;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
