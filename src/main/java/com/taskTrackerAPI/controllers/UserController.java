0ea653319c92a192db6979db2e0a7ba1d51924c1
Filipe <filipej.f.1020@gmail.com>
2025-01-30 20:12:34 -0300

    auth methods added

--- src/main/java/com/taskTrackerAPI/controllers/UserController.java
+++ src/main/java/com/taskTrackerAPI/controllers/UserController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/users")
-public class UserController {
+public class    UserController {
 
     @Autowired
     private UserService userService;
